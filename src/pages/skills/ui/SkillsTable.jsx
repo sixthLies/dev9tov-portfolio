d00aab9d54e6c0dc154cd9e251343b06873127e9
Sixthlies <prodbyxantha.@gmail.com>
2026-01-29 20:19:15 +0300

    рефакторинг SkillsTable

--- src/pages/skills/ui/SkillsTable.jsx
+++ src/pages/skills/ui/SkillsTable.jsx
@@ -1,5 +1,33 @@
 import { resources } from "../model/pageConfig"
 
+const GroupTitle = ({ titleClassName, title, img }) => {
+  return (
+    <h3 className={titleClassName}>
+      {title}
+      <img className="group__img" src={img} alt="Logo" />
+    </h3>
+  )
+}
+
+const SkillItemLink = ({ item, linkClassName, itemClassName }) => {
+  const resource = resources[item.key]
+  if (!resource) return null
+
+  return (
+    <li className={itemClassName} key={item.key}>
+      <a
+        className={linkClassName}
+        href={resource.href}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="skills-table__img" src={resource.logo} alt="itemLogo" />
+        {item.name}
+      </a>
+    </li>
+  )
+}
+
 export const SkillsTable = ({
   skillsInfo,
   root,
@@ -10,46 +38,35 @@ export const SkillsTable = ({
   tagsList,
   tagsListSecondary,
   link,
-}) => (
-  <section className={root}>
-    {skillsInfo.map((block) => (
-      <section className={group} key={block.id}>
-        <h3 className={title}>
-          {block.title}
-          <img className="group__img" src={block.img} alt="Logo" />
-        </h3>
+}) => {
+  return (
+    <section className={root}>
+      {skillsInfo.map((block) => (
+        <section className={group} key={block.id}>
+          <GroupTitle
+            title={block.title}
+            titleClassName={title}
+            img={block.img}
+          />
 
-        {block.groups.map((group) => (
-          <div className={row} key={group.label}>
-            <strong className={label}>{group.label}</strong>
+          {block.groups.map((skillGroup) => (
+            <div className={row} key={skillGroup.label}>
+              <strong className={label}>{skillGroup.label}</strong>
 
-            <ul className={tagsList}>
-              {group.items.map((item) => {
-                const resourse = resources[item.key]
-                if (!resourse) return null
-
-                return (
-                  <li className={tagsListSecondary} key={item.name}>
-                    <a
-                      className={link}
-                      href={resourse.href}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img
-                        className="skills-table__img"
-                        src={resourse.logo}
-                        alt="itemLogo"
-                      />
-                      {item.name}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        ))}
-      </section>
-    ))}
-  </section>
-)
+              <ul className={tagsList}>
+                {skillGroup.items.map((item) => (
+                  <SkillItemLink
+                    key={item.key}
+                    item={item}
+                    linkClassName={link}
+                    itemClassName={tagsListSecondary}
+                  />
+                ))}
+              </ul>
+            </div>
+          ))}
+        </section>
+      ))}
+    </section>
+  )
+}
