9bc54303b8391541ff3ff69f966989ea0d5710db
Sixthlies <prodbyxantha.@gmail.com>
2026-01-29 20:05:24 +0300

    обновление импортов

--- src/pages/skills/ui/SkillsPage.jsx
+++ src/pages/skills/ui/SkillsPage.jsx
@@ -1,9 +1,9 @@
-import { SkillsTable } from "./SkillsTable"
 import { InfoBlock, LinksBlock } from "@/shared/ui"
-import { skillsClasses } from "./model/classes"
-import { infoBoxSkillsData } from "./model/infoBox"
 import { menuItems } from "@/shared/config/navigation"
-import { skillsInfo } from "./model/pageConfig"
+import { SkillsTable } from "./SkillsTable"
+import { infoBoxSkillsData } from "../model/infoBox"
+import { skillsInfo } from "../model/pageConfig"
+import { skillsClasses } from "../model/classes"
 
 export const SkillsPage = () => {
   const primaryLinks = ["/about", "/projects"]
