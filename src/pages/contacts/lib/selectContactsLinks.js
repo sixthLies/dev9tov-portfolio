2d5d3104943ce4ae0ceb978f418397cbc6b404fb
Sixthlies <prodbyxantha.@gmail.com>
2026-01-31 19:23:58 +0300

    Убрал комментарий

--- src/pages/contacts/lib/selectContactsLinks.js
+++ src/pages/contacts/lib/selectContactsLinks.js
@@ -1,7 +1,6 @@
 import { menuItems } from "@/shared/config/navigation"
 import { CONTACTS_ALLOWED_LINK_HREFS } from "../model/constants"
 
-// Селектор: инкапсулирует правило выбора ссылок для страницы Contacts
 export const selectContactsLinks = () => {
   const allowed = new Set(CONTACTS_ALLOWED_LINK_HREFS)
   return menuItems.filter((link) => allowed.has(link.href))
