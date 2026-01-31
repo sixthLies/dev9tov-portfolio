39f504931ad79face31095241cacc62b92ace6e0
Sixthlies <prodbyxantha.@gmail.com>
2026-01-31 16:01:20 +0300

    Исправил несоответствие classNames

--- src/pages/not-found/ui/NotFoundPage.jsx
+++ src/pages/not-found/ui/NotFoundPage.jsx
@@ -1,21 +1,25 @@
 import { Link } from "react-router"
 import { images } from "@/shared/assets"
 import { NOT_FOUND } from "../model/constants"
-import { notFounfClasses } from "../model/classes"
+import { notFoundClasses } from "../model/classes"
 
 export const NotFoundPage = () => {
+  const { pageClass, blockClass, buttonClass, textClass } = {
+    ...notFoundClasses,
+  }
+
   return (
-    <section className={notFounfClasses}>
+    <section className={pageClass}>
       <img
         width={`${NOT_FOUND.imageWidthPx}px`}
         src={images.notFoundPageImg}
         alt={NOT_FOUND.imageAlt}
       />
 
-      <div className={NOT_FOUND.blockClass}>
-        <p className={NOT_FOUND.textClass}>{NOT_FOUND.message}</p>
+      <div className={blockClass}>
+        <p className={textClass}>{NOT_FOUND.message}</p>
 
-        <Link className={NOT_FOUND.buttonClass} to={NOT_FOUND.homePath}>
+        <Link className={buttonClass} to={NOT_FOUND.homePath}>
           {NOT_FOUND.homeLinkText}
         </Link>
       </div>
