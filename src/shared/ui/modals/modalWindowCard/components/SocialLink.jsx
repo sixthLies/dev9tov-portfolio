0d116efc381a875aacc06b5980c23f20c05c3373
Sixthlies <prodbyxantha.@gmail.com>
2026-02-26 12:50:35 +0300

    Добавил rel атрибут

--- src/shared/ui/modals/modalWindowCard/components/SocialLink.jsx
+++ src/shared/ui/modals/modalWindowCard/components/SocialLink.jsx
@@ -1,7 +1,12 @@
 import { SOCIAL_LINK_PREVIEW_LENGTH } from "../constants"
 
 export const SocialLink = ({ href }) => (
-  <a className="social-card__link" href={href} target="_blank">
+  <a
+    className="social-card__link"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     {href && href.slice(0, SOCIAL_LINK_PREVIEW_LENGTH)}
   </a>
 )
