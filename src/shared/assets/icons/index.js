b8c74d097a3d7fdc8192bf53bddffdfb53aa0bec
Sixthlies <prodbyxantha.@gmail.com>
2026-04-19 05:51:26 +0300

    Оптимизировал размеры файлов/иконок на странице skills

--- src/shared/assets/icons/index.js
+++ src/shared/assets/icons/index.js
@@ -7,16 +7,15 @@ import gmail from "./socials/gmail.svg"
 import telegram from "./socials/telegram.svg"
 
 // frontend
-// png
-import htmlLogo from "./skills/frontend/webp/html.webp"
-import cssLogo from "./skills/frontend/webp/css.webp"
-import jsLogo from "./skills/frontend/webp/js.webp"
-import reactLogo from "./skills/frontend/webp/react.webp"
-import sassLogo from "./skills/frontend/webp/sass.webp"
-import viteLogo from "./skills/frontend/webp/vite.webp"
-import reduxLogo from "./skills/frontend/webp/redux.webp"
-import postgresqlLogo from "./skills/frontend/webp/postgresql.webp"
 // svg
+import sassLogo from "./skills/frontend/svg/sass.svg"
+import reduxLogo from "./skills/frontend/svg/redux.svg"
+import reactLogo from "./skills/frontend/svg/react.svg"
+import postgresqlLogo from "./skills/frontend/svg/postgresql.svg"
+import jsLogo from "./skills/frontend/svg/js.svg"
+import htmlLogo from "./skills/frontend/svg/html.svg"
+import cssLogo from "./skills/frontend/svg/css.svg"
+import viteLogo from "./skills/frontend/svg/vite.svg"
 import reactRouterLogo from "./skills/frontend/svg/react-router.svg"
 import tailwindLogo from "./skills/frontend/svg/tailwind.svg"
 import axiosLogo from "./skills/frontend/svg/axios.svg"
@@ -26,13 +25,16 @@ import dockerLogo from "./skills/frontend/svg/docker.svg"
 import n8nLogo from "./skills/frontend/svg/n8n.svg"
 import postmanLogo from "./skills/frontend/svg/postman.svg"
 import windowsLogo from "./skills/frontend/svg/windows.svg"
+import ubuntuLogo from "./skills/frontend/svg/ubuntu.svg"
+import powershellLogo from "./skills/frontend/svg/powershell.svg"
+import figmaLogo from "./skills/frontend/svg/figma.svg"
+import jsonLogo from "./skills/frontend/svg/json.svg"
 
 // ai
-// png
-import qwenLogo from "./skills/ai/webp/qwen.webp"
 // svg
-import fluxLogo from "./skills/ai/svg/flux.svg"
-import nanoBananaLogo from "./skills/ai/svg/nano-banana.svg"
+import qwenLogo from "./skills/ai/svg/qwen.svg"
+import civitaiLogo from "./skills/ai/svg/civitai.svg"
+import aistudioLogo from "./skills/ai/svg/aistudio.svg"
 import geminiLogo from "./skills/ai/svg/gemini.svg"
 import grokLogo from "./skills/ai/svg/grok.svg"
 import midjourneyLogo from "./skills/ai/svg/midjourney.svg"
@@ -41,18 +43,17 @@ import soraLogo from "./skills/ai/svg/sora.svg"
 import comfyLogo from "./skills/ai/svg/comfy.svg"
 import ghatGPTLogo from "./skills/ai/svg/ghatGPT.svg"
 import perplexityLogo from "./skills/ai/svg/perplexity.svg"
-import notebooklmLogo from "./skills/ai/svg/notebooklm.svg"
 import claudeLogo from "./skills/ai/svg/claude.svg"
 import ollamaLogo from "./skills/ai/svg/ollama.svg"
 
 // software
 // webp
-import photoshopLogo from "./skills/softwares/webp/photoshop.webp"
 import flstudioLogo from "./skills/softwares/webp/flstudio.webp"
-import jiraLogo from "./skills/softwares/webp/jira.webp"
 import onecLogo from "./skills/softwares/webp/1C.webp"
 import exelLogo from "./skills/softwares/webp/exel.webp"
 // svg
+import photoshopLogo from "./skills/softwares/svg/photoshop.svg"
+import jiraLogo from "./skills/softwares/svg/jira.svg"
 import premiereproLogo from "./skills/softwares/svg/premierepro.svg"
 import vsstudioLogo from "./skills/softwares/svg/vsstudio.svg"
 import procreateLogo from "./skills/softwares/svg/procreate.svg"
@@ -87,12 +88,16 @@ export {
   dockerLogo,
   postmanLogo,
   windowsLogo,
+  powershellLogo,
+  ubuntuLogo,
+  jsonLogo,
+  figmaLogo,
 
   // ai
+  aistudioLogo,
+  civitaiLogo,
   comfyLogo,
   qwenLogo,
-  fluxLogo,
-  nanoBananaLogo,
   geminiLogo,
   grokLogo,
   midjourneyLogo,
@@ -100,7 +105,6 @@ export {
   soraLogo,
   ghatGPTLogo,
   perplexityLogo,
-  notebooklmLogo,
   claudeLogo,
   ollamaLogo,
 
