176bbd2dbf974c12bdaa56b6d94d6e60c8b7d781
Sixthlies <prodbyxantha.@gmail.com>
2026-04-19 05:49:14 +0300

    Убрал лишний infobox

--- src/pages/skills/model/infoBox.js
+++ src/pages/skills/model/infoBox.js
@@ -2,10 +2,5 @@ import { infoTitle } from "@/shared/const/infoTitle"
 
 export const infoBoxSkillsData = {
   title: infoTitle,
-  info: "Нейросети, которые чаще всего использую, отмеченны значком 🔥.",
-}
-
-export const secondInfoBoxSkillsData = {
-  title: infoTitle,
   info: "При нажатии на технолигию/программу происходит переадрессация на официальный сайт технологии/программы.",
 }
