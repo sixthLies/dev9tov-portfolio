f90542536395f29b0ad10e16a2cb365ecf3f1b91
Sixthlies <prodbyxantha.@gmail.com>
2026-04-21 11:53:05 +0300

    Текстовые правки

--- src/pages/about/ui/sections/GoalsSection/ui/GoalsHeader.jsx
+++ src/pages/about/ui/sections/GoalsSection/ui/GoalsHeader.jsx
@@ -2,7 +2,7 @@ export const GoalsHeader = ({ head, title, subtitle }) => {
   return (
     <header className={head}>
       <h2 className={title}>Цели</h2>
-      <p className={subtitle}>В каких направлениях работы с AI двигаюсь:</p>
+      {/* <p className={subtitle}>В каких направлениях работы с AI двигаюсь:</p> */}
     </header>
   )
 }
